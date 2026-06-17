76a7133eef9f1149adb31443a688f00b07aa6276
codesystemweb <codesystemweb@gmail.com>
2026-06-17 18:18:30 -0300

    v1

--- css/js/main.js
+++ css/js/main.js
@@ -124,6 +124,21 @@ document.querySelectorAll('.menu a')
 
 });
 
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item=>{
+
+    const question =
+    item.querySelector('.faq-question');
+
+    question.addEventListener('click',()=>{
+
+        item.classList.toggle('active');
+
+    });
+
+});
+
 // BOTÃO WHATSAPP FUTURO
 
 console.log("VOO Telecom carregado");
